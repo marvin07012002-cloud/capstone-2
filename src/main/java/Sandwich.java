db87cf1dc40d64c218a40ee54e624911b504b9a3
marbin701 <marvin07012002@gmail.com>
2026-05-22 12:02:16 -0400

    Fields added to Sandwich class

--- src/main/java/Sandwich.java
+++ src/main/java/Sandwich.java
@@ -1,2 +1,13 @@
+import java.util.ArrayList;
+
 public class Sandwich {
+    private BreadType breadType;
+    private Size size;
+    private Meat meat;
+    private Cheese cheese;
+    private ArrayList<Toppings> toppings;
+    private Side side;
+    private boolean toasted;
+
+
 }
