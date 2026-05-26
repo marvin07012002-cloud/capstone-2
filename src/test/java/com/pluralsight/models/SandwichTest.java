909e932380716c633d053932b938bb3ec60a045f
marbin701 <marvin07012002@gmail.com>
2026-05-26 11:12:20 -0400

    SandwichTest returnsBasePrice added

--- src/test/java/com/pluralsight/models/SandwichTest.java
+++ src/test/java/com/pluralsight/models/SandwichTest.java
@@ -1,7 +1,21 @@
 package com.pluralsight.models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SandwichTest {
+
+    @Test
+    void returnsBasePrice (){
+        Sandwich sandwich = new Sandwich();
+
+        sandwich.setSize(Size.LARGE);
+        sandwich.setBreadType(BreadType.WRAP);
+
+        double priceReturn = sandwich.getPrice();
+
+        assertEquals(8.50,priceReturn);
+    }
 
 }
