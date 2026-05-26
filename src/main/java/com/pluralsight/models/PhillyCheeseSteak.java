366adb84a36fc8c481458cab903e9eef21c8ae1f
marbin701 <marvin07012002@gmail.com>
2026-05-25 20:31:21 -0400

    Philly Cheese Steak extends to Sandwich and setter added

--- src/main/java/com/pluralsight/models/PhillyCheeseSteak.java
+++ src/main/java/com/pluralsight/models/PhillyCheeseSteak.java
@@ -4,5 +4,11 @@ public class PhillyCheeseSteak extends Sandwich{
 
     public PhillyCheeseSteak(){
         setSize(Size.MEDIUM);
+        setBreadType(BreadType.WHITE);
+        setMeat(Meat.STEAK);
+        setCheese(Cheese.AMERICAN);
+        addTopping(Toppings.PEPPERS);
+        addSauce(Sauces.MAYO);
+        setToasted(true);
     }
 }
