437d7c7f30d7e036bd4f36fb4d0c3acf33514380
marbin701 <marvin07012002@gmail.com>
2026-05-26 11:20:08 -0400

    OrderTest testingOrderReturn added

--- src/test/java/com/pluralsight/models/OrderTest.java
+++ src/test/java/com/pluralsight/models/OrderTest.java
@@ -1,7 +1,26 @@
 package com.pluralsight.models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
+    @Test
+    void testingOrderReturn() {
+        Order order = new Order();
+        Sandwich sandwich = new Sandwich();
+        Drink drink = new Drink(Size.LARGE);
+        Chips chips = new Chips();
+
+        sandwich.setSize(Size.LARGE);
+
+        order.addSandwich(sandwich);
+        order.addDrink(drink);
+        order.addChips(chips);
+
+        double totalMoney = order.getTotalPrice();
+
+        assertEquals(13 , totalMoney);
+    }
 
 }
