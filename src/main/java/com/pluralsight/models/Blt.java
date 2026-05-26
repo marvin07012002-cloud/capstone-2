071d3d0ef1cce6b9c337ed8641a70aaee26f170c
marbin701 <marvin07012002@gmail.com>
2026-05-25 20:26:55 -0400

    Blt class extends to Sandwich class and setter added

--- src/main/java/com/pluralsight/models/Blt.java
+++ src/main/java/com/pluralsight/models/Blt.java
@@ -2,4 +2,14 @@ package com.pluralsight.models;
 
 public class Blt extends Sandwich{
 
+    public Blt(){
+        setSize(Size.MEDIUM);
+        setBreadType(BreadType.WHITE);
+        setMeat(Meat.BACON);
+        setCheese(Cheese.CHEDDAR);
+        addTopping(Toppings.LETTUCE);
+        addTopping(Toppings.TOMATOES);
+        addSauce(Sauces.RANCH);
+        setToasted(true);
+    }
 }
