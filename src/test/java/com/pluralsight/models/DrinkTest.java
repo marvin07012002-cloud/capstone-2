eceab2381e5e9f53f64beb62da258afabfc7ae4e
marbin701 <marvin07012002@gmail.com>
2026-05-26 11:23:35 -0400

    DrinkTest drinkReturn added

--- src/test/java/com/pluralsight/models/DrinkTest.java
+++ src/test/java/com/pluralsight/models/DrinkTest.java
@@ -1,7 +1,19 @@
 package com.pluralsight.models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DrinkTest {
+
+    @Test
+    void drinkReturn(){
+        Drink drink = new Drink(Size.SMALL);
+
+        double drinkPrice =drink.getPrice();
+
+        assertEquals(2,drinkPrice);
+
+    }
 
 }
